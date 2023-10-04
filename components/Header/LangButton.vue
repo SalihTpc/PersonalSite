123b9cc4fce48b6c35cc8cc543422d9e3eee244b
SalihTpc <salihtopcu@hotmail.com.tr>
2023-10-04 13:50:35 +0300

    lang button added.

---
+++ components/Header/LangButton.vue
@@ -0,0 +1,20 @@
+<script setup>
+const { locale, locales, setLocale } = useI18n();
+
+const availableLocales = computed(() => {
+  return locales.value.filter((i) => i.code !== locale.value);
+});
+</script>
+
+<template>
+  <div>
+    <button
+      v-for="locale in availableLocales"
+      :key="locale.code"
+      @click.prevent.stop="setLocale(locale.code)"
+      class="ml-2 font-montserrat font-semibold rounded-full px-1 py-0.5 sm:px-2 sm:py-1 bg-slate-300 hover:bg-slate-400 hover:font-bold dark:bg-black dark:text-slate-500"
+    >
+      {{ locale.name }}
+    </button>
+  </div>
+</template>
